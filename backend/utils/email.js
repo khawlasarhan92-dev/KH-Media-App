8db7db4900d4b99075cb80d48d5c699b889bfa97
User Name <user@gmail.com>
2025-10-12 01:48:48 +0300

    Update: change Gmail service configuration in backend

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -2,14 +2,16 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async(options) =>{
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+     host: 'smtp.gmail.com', 
+    port: 465, 
+    secure: true,
     auth:{
       user:process.env.EMAIL,
       pass:process.env.EMAIL_PASS,
     },
   });
   const mailOptions = {
-    form:`"media-app" Share Your Imagination`,
+    from:`"media-app" Share Your Imagination`,
     to:options.email,
     subject:options.subject,
     html:options.html,
